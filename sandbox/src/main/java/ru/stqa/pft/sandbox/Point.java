ef226dea0bb302a0cb470c3d89fb13ca6e715da4
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-12 20:06:15 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,12 +3,18 @@ package ru.stqa.pft.sandbox;
 public class Point {
     public double x1, y1, x2, y2;
 
+    public Point(double x1,double y1,double x2,double y2){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
     /**
      * @return = Вывод расчета длины отрезка по теореме Пифагора.
      * Для расчета расмотрим прямоугольный треугольник, т.е. "p1 = x1 - x2, p2 = y1 - y2"
      */
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x2-p1.x1)*(p2.x2-p1.x1)+(p2.y2-p1.y1)*(p2.y2-p1.y1));
+    public double distance() {
+        return Math.sqrt((this.x2-this.x1)*(this.x2-this.x1)+(this.y2-this.y1)*(this.y2-this.y1));
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Runner.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Runner.java
@@ -1,17 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-import static ru.stqa.pft.sandbox.Point.distance;
-
 public class Runner {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point();
-
-        p1.x1 = 4;
-        p1.y1 = 4;
-        p2.x2 = 6;
-        p2.y2 = 6;
-
-        System.out.println("Расстояние между точками: " + distance(p1,p2));
+     Point a = new Point(1,2,3,4);
+        System.out.println("Расстояние между точками: "+ a.distance());
     }
 }
