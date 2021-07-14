af699949724379cb2d996f623609c3add81f9e55
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-14 22:56:01 +0300

    Extends

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GruopeCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GruopeCreationTests.java
@@ -1,29 +1,9 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GruopeCreationTests {
-  private WebDriver wd;
+public class GruopeCreationTests extends StepTesting{
 
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
 
   @Test
   public void testGruopeCreation() throws Exception {
@@ -32,99 +12,8 @@ public class GruopeCreationTests {
     fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
     reternToGroupePage();
-    addNewContact();
-    setNameContact(new NameNewContact("Qqwerty", "Voiue", "Fret"));
-    setNickName("Krevedko");
-    setHome( "New Bitch");
-    gotoHomePage();
-  }
 
-  private void reternToGroupePage() {
-    wd.findElement(By.xpath("//div[@id='content']/form/span")).click();
-  }
-
-  private void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  private void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  private void addNewContact() {
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-  }
-
-  private void setNameContact(NameNewContact nameNewContact) {
-    wd.findElement(By.name("firstname")).sendKeys(nameNewContact.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(nameNewContact.getMiddleName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(nameNewContact.getLastName());
-  }
-  private void setNickName(String nickName) {
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(nickName);
-  }
-
-  private void setHome(String s) {
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(s);
-  }
-
-  private void gotoHomePage() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    wd.findElement(By.linkText("home page")).click();
   }
 
 
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    logoutPage();
-    wd.quit();
-  }
-
-  private void logoutPage() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
