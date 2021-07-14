0982cb26e2014102c4d837fc5bd13064a99cfa4a
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-14 20:14:03 +0300

    Лекция 2.2. Делаем тест для веб-приложения, используя драйвер Selenium

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GruopeCreationTests.java
@@ -0,0 +1,63 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GruopeCreationTests {
+  private WebDriver wd;
+
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testGruopeCreation() throws Exception {
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    wd.findElement(By.linkText("groups")).click();
+    wd.findElement(By.name("new")).click();
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys("test1");
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys("test2");
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys("test3");
+    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.xpath("//div[@id='content']/form/span")).click();
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Runner.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Runner.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class Runner {
     public static void main(String[] args) {
-     Point a = new Point(1,2,3,4);
+     Point a = new Point(-1,-2,-3,-4);
         System.out.println("Расстояние между точками: "+ a.distance());
     }
 }
