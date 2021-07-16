45d5b6ff4420bbe3bf9eee99d4385a94d2a3330f
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-16 16:06:42 +0300

    Extends

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NameNewContact.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.addressbook;
+
+public class NameNewContact {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+
+    public NameNewContact(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+}
