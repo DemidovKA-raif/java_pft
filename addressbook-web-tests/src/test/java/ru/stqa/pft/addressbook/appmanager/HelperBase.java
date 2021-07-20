0086a42ed73bdb21e23bd5c6ce68481641816139
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-20 22:15:59 +0300

    Задание 7. кроссплатформенность

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class HelperBase {
     protected WebDriver wd;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class NavigationHelper extends HelperBase {
 
