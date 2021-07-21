4314f7a1b79c135d652c70cbc29e558e510b246f
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-21 16:08:29 +0300

    Задание 2. Переделка

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Runner.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Runner.java
@@ -2,7 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class Runner {
     public static void main(String[] args) {
-     Point a = new Point(-1,-2,-3,-4);
-        System.out.println("Расстояние между точками: "+ a.distance());
+        Point p1 = new Point(1, 5);
+        Point p2 = new Point(10, 10);
+        System.out.println("Расстояние между точками: " + p1.distance(p2));
     }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/TestPoint.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/TestPoint.java
@@ -4,17 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestPoint {
+    Point point = new Point(10, 10);
+
 
     @Test
     public void testPoint1 () {
-        Point a = new Point(1,2,3,4);
-        Assert.assertEquals(a.distance(),2.8284271247461903 );
+        Point a = new Point(1,5);
+        Assert.assertEquals(point.distance(a),10.295630140987);
     }
 
     @Test
     public void testPoint2 () {
-        Point a = new Point(1, 2, 3, 4);
-        if (a.distance() != 0) {
+        Point a = new Point( 3, 4);
+        if (point.distance(a) != 0) {
             System.out.println("It is OKay!");
         } else {
             System.out.println("Отрицательное значение!");
