feba3768f865a9d6bb83439118c061a8885b899f
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-07-21 16:09:01 +0300

    Задание 2. Переделка

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,20 +1,14 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-    public double x1, y1, x2, y2;
+    private double x, y;
 
-    public Point(double x1,double y1,double x2,double y2){
-        this.x1 = x1;
-        this.x2 = x2;
-        this.y1 = y1;
-        this.y2 = y2;
-    }
-    /**
-     * @return = Вывод расчета длины отрезка по теореме Пифагора.
-     * Для расчета расмотрим прямоугольный треугольник, т.е. "p1 = x1 - x2, p2 = y1 - y2"
-     */
-    public double distance() {
-        return Math.sqrt((this.x2-this.x1)*(this.x2-this.x1)+(this.y2-this.y1)*(this.y2-this.y1));
+    public Point(double x1, double y1) {
+        this.x = x1;
+        this.y = y1;
     }
 
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    }
 }
