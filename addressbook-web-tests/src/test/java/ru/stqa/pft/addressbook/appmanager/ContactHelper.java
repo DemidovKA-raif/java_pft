176bf05adaf959888b8e759bed50458d9ef033e6
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-08-05 18:03:54 +0300

    ContactPhoneTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -8,10 +8,7 @@ import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class ContactHelper extends HelperBase {
 
@@ -23,15 +20,22 @@ public class ContactHelper extends HelperBase {
         click(By.linkText("add new"));
     }
 
-
+    public ContactData infoFromEditForm(ContactData contact) {
+        modificationContact(contact.getId());
+        String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+        String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+        String home = wd.findElement(By.name("home")).getAttribute("value");
+        String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+        String work = wd.findElement(By.name("work")).getAttribute("value");
+        wd.navigate().back();
+        return new ContactData().withId(contact.getId()).withFirstName(firstname).withLastName(lastname).withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+    }
 
     public void setNameContact(ContactData contactData, boolean creation) {
         type(By.name("firstname"), contactData.getFirstName());
         type(By.name("middlename"), contactData.getMiddleName());
         type(By.name("lastname"), contactData.getLastName());
         type(By.name("nickname"), contactData.getNickName());
-
-
         /**
          * Выбор элемента из всплывающего списка
          */
@@ -69,6 +73,7 @@ public class ContactHelper extends HelperBase {
         contactCache = null;
         gotoHomePage();
     }
+
     public void delete(ContactData contact) throws InterruptedException {
         selectContactById(contact.getId());
         clickDeletionContact();
@@ -78,7 +83,7 @@ public class ContactHelper extends HelperBase {
     }
 
     public void modificationContact(int id) {
-        wd.findElement(By.xpath("table[@id='maintable']/tbody/tr["+ id +"]/td[8]/a/img")).click();
+        wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id))).click();
     }
 
     public void selectContactById(int id) {
@@ -104,16 +109,21 @@ public class ContactHelper extends HelperBase {
     private Contacts contactCache = null;
 
     public Contacts all() {
-        if (contactCache != null){
+        if (contactCache != null) {
             return new Contacts(contactCache);
         }
         contactCache = new Contacts();
         List<WebElement> elements = wd.findElements(By.name(("entry")));
         for (WebElement element : elements) {
+            List<WebElement> cells = element.findElements(By.tagName("td"));
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            String firstname = element.findElement(By.xpath("td[3]")).getText();
-            String lastName = element.findElement(By.xpath("td[2]")).getText();
-            contactCache.add(new ContactData().withId(id).withFirstName(firstname).withMiddleName(null).withLastName(lastName).withMyHome(null).withGroup(null).withNickName(null));
+            String firstname = cells.get(1).getText();
+            String lastName = cells.get(2).getText();
+            String[] phones = cells.get(5).getText().split("\n");
+            contactCache.add(new ContactData().withId(id).withFirstName(firstname).withMiddleName(null).withLastName(lastName).withMyHome(null).withGroup(null).withNickName(null)
+                    .withHomePhone(phones[0])
+                    .withMobilePhone(phones[1])
+                    .withWorkPhone(phones[2]));
         }
         return contactCache;
     }
