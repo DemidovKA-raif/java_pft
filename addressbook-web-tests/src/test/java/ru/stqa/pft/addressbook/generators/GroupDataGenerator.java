05729eb141285fe2aa90716fa3c2aa9d005acad9
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-08-10 17:49:44 +0300

    All tests is work

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,15 +14,31 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
 
-        List<GroupData> groups = generaterGroups(count);
-        save(groups, file);
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws Exception {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws Exception{
+        List<GroupData> groups = generaterGroups(count);
+        save(groups, new File(file));
+    }
+
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println();
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
@@ -28,7 +47,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generaterGroups(int count) {
+    private  List<GroupData> generaterGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
             groups.add(new GroupData().withName(String.format("test %s", i))
