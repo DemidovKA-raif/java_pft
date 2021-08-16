fb5b7d129724535da090ed1cae616b594331745f
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-08-16 20:59:51 +0300

    Connect DB

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DBConnectionTest.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+import java.sql.*;
+
+public class DBConnectionTest {
+    @Test
+    public void testDBConnection(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+            Groups groups = new Groups();
+            while (resultSet.next()){
+                groups.add(new GroupData().withId(resultSet.getInt("group_id")).withName(resultSet.getString("group_name"))
+                        .withHeader(resultSet.getString("group_header")).withFooter(resultSet.getString("group_footer")));
+            }
+            resultSet.close();
+            statement.close();
+            conn.close();
+
+            System.out.println(groups);
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
