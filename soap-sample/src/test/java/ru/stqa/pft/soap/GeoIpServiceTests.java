e1bdc4652e3c12fca3a548b8a97c9196248ce189
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-08-31 13:17:32 +0300

    soap

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.soap;
+
+import org.testng.annotations.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+    @Test
+    public void testMyIP(){
+
+        String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("212.34.48.80");
+        assertEquals(ipLocation.getBytes(StandardCharsets.UTF_8), "RU");
+    }
+}
+}
