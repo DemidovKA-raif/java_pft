2b1c4a9a5d35823f927aa6cc88b7913d43df556c
DemidovKA-raif <87029580+DemidovKA-raif@users.noreply.github.com>
2021-09-08 17:08:50 +0300

    selenium server

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,13 +1,17 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -33,12 +37,17 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        }
-
+if("".equals(properties.getProperty("selenium.server"))) {
+    if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+    }
+}else {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName(browser);
+    wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+}
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.BaseURL"));
         groupHelper = new GroupHelper(wd);
